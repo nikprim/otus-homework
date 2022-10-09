481917b6582b9196c829ecde64f82f95b8170b96
nikita <nikita.karachjov@vseinstrumenti.ru>
2022-10-09 21:31:57 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	word := "Hello, OTUS!"
+
+	fmt.Println(stringutil.Reverse(word))
 }
